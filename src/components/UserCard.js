8e26d8462efd9eb7232d527954b59f067232ee71
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-01 15:03:10 +0300

    Fixed UserCard component to render user details correctly and updated routing for user navigation

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";  // Import Link from react-router-dom
 
-
-function UserCard({user}) {
+function UserCard({ user }) {
   return (
     <article>
-        <h2>{user.name}</h2>
+      <h2>{user.name}</h2>
+      <p>
+        {/* Link to the profile page of the user based on their ID */}
+        <Link to={`/profile/${user.id}`}>View profile</Link>
+      </p>
     </article>
   );
 };
